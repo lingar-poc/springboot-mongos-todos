fcfb8904ea76095a0987511397cbc8f0be28ce13
lingar <yimprogramming@gmail.com>
2021-02-10 16:14:23 +0200

    Adding getById

--- src/main/java/lingar/poc/springbootmongostodos/controllers/WebServices.java
+++ src/main/java/lingar/poc/springbootmongostodos/controllers/WebServices.java
@@ -2,8 +2,11 @@ package lingar.poc.springbootmongostodos.controllers;
 
 import java.util.List;
 
+import javax.websocket.server.PathParam;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,17 +22,21 @@ public class WebServices {
 	@Autowired
 	private TodoRepository repository;
 
-	@RequestMapping(value = "/test-service", method = RequestMethod.GET)//this make it not working
+	@RequestMapping(value = "/test-service", method = RequestMethod.GET) // this make it not working
 	public String testServer() {
 		return "Server is Online";
 	}
-	
-	//http://localhost:8080/ws/getTodos
+
+	// http://localhost:8080/ws/getTodos
 	@RequestMapping(value = "/getTodos")
-	public List<Todo> getTodos(){
+	public List<Todo> getTodos() {
 		return repository.findAll();
 	}
-	
-	
-	
+
+	//http://localhost:8080/ws/getTodo/PUT_HERE_ONE_OF_UPDATED_IDS(SEE LOGS)
+	@RequestMapping(value = "/getTodo/{id}")
+	public Todo getTodo(@PathVariable(value = "id") String id) {
+		return repository.findById(id).get();
+	}
+
 }
