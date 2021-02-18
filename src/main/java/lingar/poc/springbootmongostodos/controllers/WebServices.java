b179067ccaf950a35c5ae6d8679947ca3b75ce8f
lingar <yimprogramming@gmail.com>
2021-02-18 12:02:06 +0200

    Adding delete

--- src/main/java/lingar/poc/springbootmongostodos/controllers/WebServices.java
+++ src/main/java/lingar/poc/springbootmongostodos/controllers/WebServices.java
@@ -24,7 +24,7 @@ public class WebServices {
 	@Autowired
 	private TodoRepository repository;
 
-	@RequestMapping(value = "/test-service", method = RequestMethod.GET) // this make it not working
+	@RequestMapping(value = "/test-server", method = RequestMethod.GET) // this make it not working
 	public String testServer() {
 		return "Server is Online";
 	}
@@ -41,7 +41,7 @@ public class WebServices {
 		return repository.findById(id).get();
 	}
 
-	//You can check this method with postman. 
+	// You can check this method with postman.
 	@RequestMapping(value = "/todoAction", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
 	public Todo createTodo(@RequestBody Todo newItem) {
 		try {
@@ -49,22 +49,37 @@ public class WebServices {
 			return newItem;
 
 		} catch (Exception e) {
-			//handle exception
+			// handle exception
 			System.err.println("error occurred :" + e.getMessage());
 			return null;
 		}
 
 	}
 
-	//You can check this method with postman. 
+	// You can check this method with postman.
 	@RequestMapping(value = "/todoAction", method = RequestMethod.PUT, consumes = MediaType.TEXT_PLAIN_VALUE)
-	public boolean updateTodo(@RequestBody  String id) {
+	public boolean updateTodo(@RequestBody String id) {
 		try {
 			Todo todo = repository.findById(id).get();
 			todo.setMark(!todo.isMark());
 			repository.save(todo);
-			
-			return true; 
+
+			return true;
+
+		} catch (Exception e) {
+			// handle exception
+			System.err.println("error occurred :" + e.getMessage());
+			return false;
+		}
+
+	}
+
+	// You can check this method with postman.
+	@RequestMapping(value = "/todoAction", method = RequestMethod.DELETE, consumes = MediaType.TEXT_PLAIN_VALUE)
+	public boolean deleteTodo(@RequestBody String id) {
+		try {
+			repository.deleteById(id);
+			return true;
 
 		} catch (Exception e) {
 			// handle exception
